58c5ea23398daadd30e90401bae8b87176a8cad5
Cedric Murairi <murairicedric@gmail.com>
2021-06-03 13:23:31 +0200

    set up the runner file and register routes

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express"
+import blog from "./routes/blog.js"
+
+const app = express()
+
+const port = process.env.PORT || 3001
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+app.use('/blog', blog)
+
+app.listen(port, () => {
+    console.log(`App listening on port: ${port}`)
+})

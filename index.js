3cb7b225782f13ca62fd060a8b0bf63c51d39607
Cedric Murairi <murairicedric@gmail.com>
2021-07-25 21:53:14 +0200

    integrated the authentication with app and fixed js import

--- index.js
+++ index.js
@@ -6,16 +6,20 @@ import admin from "./routes/admin.js"
 import passport from 'passport';
 import flash from 'express-flash';
 import session from 'express-session';
+import bodyParser from 'body-parser';
 import initializePassport from './passport.config.js'
+import { getUserById } from './controllers/userController.js'
+import { getUserByEmail } from './controllers/userController.js'
+import { saveMessage } from './controllers/messageController.js'
 
 const app = express()
 dotenv.config()
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3005
 initializePassport(
     passport,
-    email => userController.findUserByEmail(email),
-    id => userController.getUserById(id)
+    email => getUserByEmail(email),
+    id => getUserById(id)
 );
 
 app.set('view engine', 'ejs')
@@ -28,6 +32,7 @@ app.use(session({
 }))
 app.use(passport.initialize())
 app.use(passport.session())
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.render('index', { message: "" })
@@ -41,16 +46,19 @@ app.get('/experiences', (req, res) => {
     res.render('experiences')
 })
 
-app.post('/contact-me', (req, res) => {
-    messageController.saveMessage(req.body.name, req.body.email, req.body.title, req.body.message);
-    res.status(200).json({
-        success: true,
-        message: "Your message has been received!"
-    })
-})
-
-app.get('/login', (req, res) => {
-    res.render('adminLogin')
+app.post('/contact-me', async (req, res) => {
+    let result = await saveMessage(req.body);
+    if (result) {
+        res.status(200).json({
+            success: true,
+            message: "Your message has been received!"
+        })
+    }else{
+        res.status(400).json({
+            success: false,
+            message: "Bad Request!"
+        })
+    }
 })
 
 app.use('/blog', blog)
